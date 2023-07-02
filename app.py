754928ed5eba43aa63d39728b9de870a207c0e28
zobirofkir <zobirofkir19@gmail.com>
2023-07-02 22:07:36 +0100

    update

--- app.py
+++ app.py
@@ -7,26 +7,24 @@ def generate_password(length=8):
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-def send_email(sender, recipient, subject, message):
-    smtp_server = 'your_smtp_server'
-    smtp_port = 587
-    smtp_username = 'zobir'
-    smtp_password = 'ofkir'
-
+def send_email(sender, recipient, subject, message, smtp_server, smtp_port, smtp_username, smtp_password):
     email_message = f"Subject: {subject}\n\n{message}"
-
-    # Connect to the SMTP server and send the email
     with smtplib.SMTP(smtp_server, smtp_port) as server:
         server.starttls()
         server.login(smtp_username, smtp_password)
         server.sendmail(sender, recipient, email_message)
 
+smtp_server = 'your_smtp_server'
+smtp_port = 587
+smtp_username = 'your_username'
+smtp_password = 'your_password'
+
 password = generate_password(12)
 print("Generated Password:", password)
 
-sender = 'zobirofkir19@gmail.com'
-recipient = 'zobirofkir19@gmail.com'
+sender = 'your_email@example.com'
+recipient = 'recipient_email@example.com'
 subject = 'Generated Password'
 message = f"Your password is: {password}"
 
-send_email(sender, recipient, subject, message)
+send_email(sender, recipient, subject, message, smtp_server, smtp_port, smtp_username, smtp_password)
